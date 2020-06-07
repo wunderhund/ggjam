288ad0797a90d1aa555f9eed7e8992fd92d47e80
Craig A. Butler <wunderhund@gmail.com>
2020-06-07 12:15:56 -0600

    Add Cloudfront Distribution

--- terraform/gatsby.tf
+++ terraform/gatsby.tf
@@ -292,3 +292,64 @@ resource "aws_codebuild_webhook" "ggjam_build" {
 #
 #  events = ["push"]
 #}
+
+
+# CloudFront Distribution
+
+locals {
+  s3_origin_id = "S3-${var.site_name}"
+}
+
+resource "aws_cloudfront_distribution" "ggjam_distribution" {
+  enabled = true
+  is_ipv6_enabled = true
+  price_class = "PriceClass_100"
+  default_root_object = "index.html"
+
+  aliases = [var.site_name]
+  
+  origin {
+    domain_name = aws_s3_bucket.ggjam-website.website_endpoint
+    origin_id   = local.s3_origin_id
+    
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:925412914118:certificate/d2e793e8-535e-420d-a1d6-90bd350a7f6e"
+    minimum_protocol_version = "TLSv1.2_2018"
+    ssl_support_method = "sni-only"
+  }
+}
